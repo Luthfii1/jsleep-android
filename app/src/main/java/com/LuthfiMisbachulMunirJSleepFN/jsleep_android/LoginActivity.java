58c81acb297fe2ac53dded06a401fafd30fc8ab0
Luthfii1 <luthfi.misbachul@ui.ac.id>
2022-11-15 17:50:56 +0700

    CS Modul 8

---
+++ app/src/main/java/com/LuthfiMisbachulMunirJSleepFN/jsleep_android/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.LuthfiMisbachulMunirJSleepFN.jsleep_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView register = findViewById(R.id.RegisterLogin);
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent move = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(move);
+            }
+        });
+        Button login = findViewById(R.id.LoginButtonLogin);
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent move = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(move);
+            }
+        });
+    }
+}
